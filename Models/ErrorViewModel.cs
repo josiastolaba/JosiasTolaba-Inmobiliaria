49e349c11505aee2bce20e8b02753db83949e91c
josiastolaba <163350052+josiastolaba@users.noreply.github.com>
2025-08-21 19:23:44 -0300

    Inmobiliaria

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace INMOBILIARIA_JosiasTolaba.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
