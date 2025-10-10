f226362c72f13ad0eb18e7a968dcb72cf95e23d3
josiastolaba <163350052+josiastolaba@users.noreply.github.com>
2025-10-10 10:23:42 -0300

    IRepositorioInmueble

--- Models/IRepositorioInmueble.cs
+++ Models/IRepositorioInmueble.cs
@@ -1,23 +1,15 @@
 
 namespace INMOBILIARIA_JosiasTolaba.Models
 {
-    public interface IRepositorioInmueble : IRepositorio<Inmueble>
-    {
+        public interface IRepositorioInmueble : IRepositorio<Inmueble>
+        {
         List<Inmueble> buscar(string dato);
         int DarDeBaja(int IdInmueble);
         IList<Inmueble> ListarInmuebles();
         Inmueble InmuebleId(int IdInmueble);
-<<<<<<< Updated upstream
         int ModificarPortada(int IdInmueble, string ruta);
-=======
-
         List<Inmueble> inmueblesPorPropietario(int IdPropietario);
-
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         IList<Inmueble> obtenerPaginados(int offset, int limit);
         int contar();
-    }
+        }
 }
